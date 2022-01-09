48150e39a701ecd6ee3ce5befbfa50f3373f6d5c
Vitaliy Burdin <vlvlv.vovov@bk.ru>
2022-01-09 18:19:32 +0500

    update package react typescript

--- truffle.js
+++ truffle.js
@@ -2,10 +2,9 @@ module.exports = {
     networks: {
       development: {
         host: 'localhost',
-        port: 8545,
+        port: 7545,
         gas: 6000000,
         network_id: '*' // Match any network id
       }
     }
   };
-  
