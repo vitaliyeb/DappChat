2454b57013664be9ee7de4e897a1a727b517dfe4
Vitaliy Burdin <vlvlv.vovov@bk.ru>
2022-01-14 21:27:34 +0500

    get acc from ganache

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import {useEffect} from "react";
 import * as React from "react";
+import getWeb3 from "./util/getWeb3";
 // import * as Web3 from "web3";
 
 const App = () => {
+  useEffect(() => {
+    (async () => {
+      //@ts-ignore
+      window.huy = await getWeb3();
+    })()
+  }, []);
 
   return (
     <div>
--- src/util/getWeb3.ts
+++ src/util/getWeb3.ts
@@ -7,23 +7,19 @@ const getWeb3 = (): Promise<Web3> => {
     p = new Promise<Web3>((resolve, reject) => {
       // Wait for loading completion to avoid race conditions with web3 injection timing.
       window.addEventListener("load", () => {
-        // let web3: Web3 = (window as any).web3 as Web3;
-        //
-        // // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-        // if (typeof web3 !== "undefined") {
-        //   console.log("Using injected web3 provider");
-        //   web3 = new Web3(web3.currentProvider);
-        //   console.log('currentProvider', web3)
-        // } else {
-        //   // Fallback to localhost if no web3 injection.
-        //   console.log("No web3 instance injected, using Local web3.");
-        //   const provider = new Web3.providers.HttpProvider("http://localhost:8545");
-        //   web3 = new Web3(provider);
-        // }
-        // (window as any).web3 = web3;
+        let web3: Web3 = (window as any).web3 as Web3;
 
-        const provider = new Web3.providers.HttpProvider("http://localhost:7545");
-        const web3 = new Web3(provider);
+        // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+        if (typeof web3 !== "undefined") {
+          console.log("Using injected web3 provider");
+          web3 = new Web3(web3.currentProvider);
+          console.log('currentProvider', web3)
+        } else {
+          // Fallback to localhost if no web3 injection.
+          console.log("No web3 instance injected, using Local web3.");
+          const provider = new Web3.providers.HttpProvider("http://localhost:8545");
+          web3 = new Web3(provider);
+        }
 
         resolve(web3);
       });
