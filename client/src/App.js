f2366c3339f9b9b2752060c4dcbfa97d91283ce7
Vitaliy Burdin <vlvlv.vovov@bk.ru>
2022-01-20 21:32:13 +0500

    add init function

--- client/src/App.js
+++ client/src/App.js
@@ -5,45 +5,30 @@ import "./App.css";
 import Web3 from "web3";
 
 const App = () => {
-    const [value, setValue] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    const [contract, setContract] = useState(null);
     const [web3, setWeb3] = useState(null);
     const [account, setAccount] = useState(null);
-    const [contract, setContract] = useState(null);
-    const [greeting, setGreeting] = useState('');
-
-    useEffect(() => {
-        (async () => {
-            const web3 = await getWeb3();
-            const account = (await web3.eth.getAccounts())[0];
-            const networkId = await web3.eth.net.getId();
-            const contract = new web3.eth.Contract(ChatContract.abi, ChatContract.networks[networkId].address);
-            setWeb3(web3);
-            setAccount(account);
-            setContract(contract);
-            console.log(contract)
-            window.w = web3;
-            console.log(await contract.methods.getMessages().call());
 
 
-            // console.log(em);
-            // em.on("data", (x) => console.log("data", x));
-            // em.on("changed", (x) => console.log("changed", x));
-            // em.on("error", (x) => console.log("error", x));
-            // em.on("connected", (x) => console.log("connected", x));
-        })();
-    }, []);
+    const init = async () => {
+        const web3 = await getWeb3();
+        const account = (await web3.eth.getAccounts())[0];
+        const networkId = await web3.eth.net.getId();
+        const contract = new web3.eth.Contract(ChatContract.abi, ChatContract.networks[networkId].address);
 
-    const changeGreeting = async () => {
-        console.log(value);
-        contract.methods.pushMessage(value).send({
-            from: account
-        });
+        setContract(contract);
+        setAccount(account);
+        setWeb3(web3);
     }
 
+    useEffect(()=>{
+        init();
+    }, []);
+
+
     return (<div>
-        <h1>{ greeting }</h1>
-        <input type="text" onChange={e => setValue(e.target.value)} value={value}/>
-        <button onClick={changeGreeting}>change</button>
+
     </div>)
 };
 
--- contracts/Chat.sol
+++ contracts/Chat.sol
@@ -2,20 +2,19 @@
 pragma solidity ^0.8.0;
 
 contract Chat {
-    string public greeting;
     struct Message {
         address owner;
         string value;
     }
 
-    Message[] public messages;
+    Message[] private _messages;
 
     function getMessages() external view returns (Message[] memory) {
-        return messages;
+        return _messages;
     }
 
     function pushMessage(string memory _value) external {
-        messages.push(Message({
+        _messages.push(Message({
             owner: msg.sender,
             value: _value
         }));
