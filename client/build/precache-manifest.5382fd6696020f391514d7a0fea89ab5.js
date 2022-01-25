faa596302039d12c9ec3cd8ab3b7046552d451ef
Vitaliy Burdin <vlvlv.vovov@bk.ru>
2022-01-25 16:49:25 +0500

    build

---
+++ client/build/precache-manifest.5382fd6696020f391514d7a0fea89ab5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3c88b0b16795506c68799be243091d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81a66d48426bf1cf6a3f",
+    "url": "/static/css/main.7a9ad38e.chunk.css"
+  },
+  {
+    "revision": "a91239644bd223f10110",
+    "url": "/static/js/2.6966e1d2.chunk.js"
+  },
+  {
+    "revision": "81a66d48426bf1cf6a3f",
+    "url": "/static/js/main.c29d7e3b.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
